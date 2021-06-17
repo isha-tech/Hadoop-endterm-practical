18b7154301a0a59fa51c673fcbc40f47046b96c9
isha-tech <76205044+isha-tech@users.noreply.github.com>
2021-06-17 11:14:37 +0530

    Update MyMapper (1).java

--- Question1/MyMapper (1).java
+++ Question1/MyMapper (1).java
@@ -11,7 +11,7 @@ import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.Mapper.Context;
 
-public class MyMapper extends Mapper<LongWritable, Text, Text, IntWritable> {
+public class PrimeMapper extends Mapper<LongWritable, Text, Text, IntWritable> {
 
 	public void map(LongWritable key, Text value, Context context) throws java.io.IOException, InterruptedException {
 		String data[] = value.toString().split(",");
