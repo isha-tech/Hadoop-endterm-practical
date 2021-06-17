4474965dfeb50f8040eeb7029a9618574a0aaf95
isha-tech <76205044+isha-tech@users.noreply.github.com>
2021-06-17 11:17:17 +0530

    Update MyReducer (1).java

--- Question1/MyReducer (1).java
+++ Question1/MyReducer (1).java
@@ -9,7 +9,7 @@ import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Reducer;
 import org.apache.hadoop.mapreduce.Reducer.Context;
 
-public class MyReducer extends Reducer<Text, IntWritable, Text, IntWritable> {
+public class PrimeReducer extends Reducer<Text, IntWritable, Text, IntWritable> {
 
 	public void reduce(Text key, Iterable<IntWritable> values, Context context)
 			throws IOException, InterruptedException {
@@ -23,6 +23,6 @@ public class MyReducer extends Reducer<Text, IntWritable, Text, IntWritable> {
 				sum += value.get();
 			}
 		}
-		context.write(key, new IntWritable(sum));
 	}
+		context.write(key, new IntWritable(sum));
 }
